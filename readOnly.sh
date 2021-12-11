9ad8c971f1c3f7a5d3808dcc23a77eefaa7360d0
Xiaolin-Zhong <xz3343@nyu.edu>
2021-12-10 23:48:22 -0500

    making some edits for the report..

--- readOnly.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for ((i = 1; i <= 1024; i*=2));
-do
-    ./run $1 -r $2 $i
-    echo "block_count: $i"
-    echo ""
-done
--- run2.sh
+++ run2.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-for ((i = 1; i < 8193; i*=2));
+for ((i = 1; i <= 1024*1024; i*=2));
 do
-    ./run $1 -w $2 $i
     ./run $1 -r $2 $i
     echo "block_count: $i"
     echo ""
